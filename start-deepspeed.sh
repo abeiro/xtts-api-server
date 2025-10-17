bf46de0f210a5d33f389f2ff4e8c6adfba58145f
abeiro <augusto.beiro@gmail.com>
2025-10-17 08:50:13 +0200

    Add LD_LIBRARY_PATH for DeepSpeed execution

--- start-deepspeed.sh
+++ start-deepspeed.sh
@@ -7,6 +7,7 @@ fi
 cd /home/dwemer/xtts-api-server/
 
 source /home/dwemer/python-tts/bin/activate
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/dwemer/python-tts/lib/python3.11/site-packages/nvidia/cu13/lib/
 
 python -m xtts_api_server --deepspeed --listen &> log.txt &
 
