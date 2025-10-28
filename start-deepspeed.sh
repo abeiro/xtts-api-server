05b5812f42f6b9044ada2979d882c069d9d429d3
abeiro <augusto.beiro@gmail.com>
2025-10-28 08:02:04 +0100

    Add conditional CUDA path setup in start-deepspeed.sh
    Conditionally add CUDA paths and libraries if the directory exists.

--- start-deepspeed.sh
+++ start-deepspeed.sh
@@ -7,7 +7,14 @@ fi
 cd /home/dwemer/xtts-api-server/
 
 source /home/dwemer/python-tts/bin/activate
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/dwemer/python-tts/lib/python3.11/site-packages/nvidia/cu13/lib/
+
+# Add CUDA to PATH if the directory exists
+if [ -d "/home/dwemer/python-tts/lib/python3.11/site-packages/nvidia/cu13/lib/" ];
+then
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/dwemer/python-tts/lib/python3.11/site-packages/nvidia/cu13/lib/
+  export PATH=/home/dwemer/python-tts/lib/python3.11/site-packages/nvidia/cu13/bin:$PATH
+  export CUDA_HOME=/home/dwemer/python-tts/lib/python3.11/site-packages/nvidia/cu13
+fi
 
 python -m xtts_api_server --deepspeed --listen &> log.txt &
 
